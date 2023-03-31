682c0e6beab6311d623f1e0a9a03cb7132e31097
totalwarANGEL <n3crosh8d3@gmail.com>
2023-03-31 20:10:02 +0200

    Add mod to map

---
+++ maps/(4) SH Auge der Sahara/maps/externalmap/mapscript.lua
@@ -0,0 +1,50 @@
+-- ###################################################################################################
+-- #                                                                                                 #
+-- #                                                                                                 #
+-- #     Mapname:  Stronghold Testmap                                                                #
+-- #                                                                                                 #
+-- #     Author:   totalwarANGEL                                                                     #
+-- #                                                                                                 #
+-- #                                                                                                 #
+-- ###################################################################################################
+
+Script.Load("data\\maps\\user\\stronghold_s5mp\\loader.lua");
+
+function OnMapStart()
+    Lib.Require("module/weather/WeatherMaker");
+
+    UseWeatherSet("DesertWeatherSet");
+    AddPeriodicSummer(360);
+    LocalMusic.UseSet = MEDITERANEANMUSIC;
+
+    SetupStronghold();
+    SetupHumanPlayer(1);
+    SetupHumanPlayer(2);
+    SetupHumanPlayer(3);
+    SetupHumanPlayer(4);
+end
+
+-- -------------------------------------------------------------------------- --
+
+function GameCallback_OnGameStart()
+	Script.Load(Folders.MapTools.."Ai\\Support.lua");
+	Script.Load("Data\\Script\\MapTools\\MultiPlayer\\MultiplayerTools.lua");
+	Script.Load("Data\\Script\\MapTools\\Tools.lua");
+	Script.Load("Data\\Script\\MapTools\\WeatherSets.lua");
+	IncludeGlobals("Comfort");
+
+	MultiplayerTools.InitCameraPositionsForPlayers();
+	MultiplayerTools.SetUpGameLogicOnMPGameConfig();
+	MultiplayerTools.GiveBuyableHerosToHumanPlayer(0);
+
+	if XNetwork.Manager_DoesExist() == 0 then
+		for i=1,4,1 do
+			MultiplayerTools.DeleteFastGameStuff(i);
+		end
+		local PlayerID = GUI.GetPlayerID();
+		Logic.PlayerSetIsHumanFlag(PlayerID, 1);
+		Logic.PlayerSetGameStateToPlaying(PlayerID);
+	end
+    OnMapStart();
+end
+
