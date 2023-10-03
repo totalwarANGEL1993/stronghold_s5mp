a260b8d3ee8400fed87fd29ff407df7a8495f8b5
totalwarANGEL <n3crosh8d3@gmail.com>
2023-10-03 23:59:09 +0200

    Add map "Small Dale"

---
+++ maps/StrongholdSmallDale.lua
@@ -0,0 +1,97 @@
+SHS5MP_RulesDefinition = {
+    -- Config version (Always an integer)
+    Version = 1,
+
+    -- ###################################################################### --
+    -- #                             CONFIG                                 # --
+    -- ###################################################################### --
+
+    -- Disable standard victory condition?
+    -- (Game is not lost when the HQ falls)
+    DisableDefaultWinCondition = true,
+    -- Disable rule configuration?
+    DisableRuleConfiguration = true;
+    -- Disable game start timer?
+    -- (Requires rule config to be disabled!)
+    DisableGameStartTimer = true;
+
+    -- Peacetime in minutes
+    PeaceTime = 0,
+    -- Open up named gates on the map.
+    -- (PTGate1, PTGate2, ...)
+    PeaceTimeOpenGates = true,
+
+    -- Fill resource piles with resources
+    -- (script name becomes amout e.g. 4000 for 4000 resources)
+    MapStartFillClay = true,
+    MapStartFillStone = true,
+    MapStartFillIron = true,
+    MapStartFillSulfur = true,
+    MapStartFillWood = true,
+
+    -- Rank
+    Rank = {
+        Initial = 0,
+        Final = 7,
+    },
+
+    -- Resources
+    -- {Honor, Gold, Clay, Wood, Stone, Iron, Sulfur}
+    Resources = {0, 500, 1000, 700, 550, 300, 0},
+
+    -- Setup heroes allowed
+    AllowedHeroes = {
+        -- Dario
+        [Entities.PU_Hero1c]             = true,
+        -- Pilgrim
+        [Entities.PU_Hero2]              = true,
+        -- Salim
+        [Entities.PU_Hero3]              = true,
+        -- Erec
+        [Entities.PU_Hero4]              = true,
+        -- Ari
+        [Entities.PU_Hero5]              = true,
+        -- Helias
+        [Entities.PU_Hero6]              = true,
+        -- Kerberos
+        [Entities.CU_BlackKnight]        = true,
+        -- Mary
+        [Entities.CU_Mary_de_Mortfichet] = true,
+        -- Varg
+        [Entities.CU_Barbarian_Hero]     = true,
+        -- Drake
+        [Entities.PU_Hero10]             = true,
+        -- Yuki
+        [Entities.PU_Hero11]             = true,
+        -- Kala
+        [Entities.CU_Evil_Queen]         = true,
+    },
+
+    -- ###################################################################### --
+    -- #                            CALLBACKS                               # --
+    -- ###################################################################### --
+
+    -- Called when map is loaded
+    OnMapStart = function()
+        UseWeatherSet("EuropeanWeatherSet");
+        AddPeriodicSummer(360);
+        AddPeriodicRain(90);
+
+        ForbidTechnology(Technologies.B_Bridge, 1);
+        ForbidTechnology(Technologies.B_Palisade, 1);
+        ForbidTechnology(Technologies.B_Wall, 1);
+    end,
+
+    -- Called after game start timer is over
+    OnGameStart = function()
+    end,
+
+    -- Called after peacetime is over
+    OnPeaceTimeOver = function()
+    end,
+
+    -- Called after game has been loaded (singleplayer)
+    OnSaveLoaded = function()
+    end,
+}
+
