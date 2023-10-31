bed2cc3883894ce19cb947cb2f85f6fb03a7d9d3
totalwarANGEL <n3crosh8d3@gmail.com>
2023-10-31 16:44:11 +0100

    Fix coast jewel

--- maps/(4) SH Kuestenjuwelen.s5x.unpacked/maps/externalmap/mapscript.lua
+++
@@ -1,8 +0,0 @@
-local MapName = Framework.GetCurrentMapName();
-local Path = "data\\maps\\user\\shs5mp_configs\\" ..MapName.. ".lua";
-local ShPath = "data\\script\\stronghold\\sh.loader.lua";
-Script.Load(ShPath);
-if gvStrongholdLoaded then
-    Script.Load(Path);
-end
-
